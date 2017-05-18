c0b438e6e8cab5019331ed79e5e9fd262c77e1c4
Trevor Lyon <trevor.lyon@icloud.com>
2017-05-18 16:54:47 -0600

    Implemented addUser and addSubmission

--- js/db.js
+++ js/db.js
@@ -80,233 +80,110 @@ module.exports = {
 
     // POST methods
     /*
-    * @param user: json object of a user. Specified above
-    * @return: returns 0 if the user already exists
-    *                  1 if the user was inserted
-    * */
+     * @param user: json object of a user. Specified above
+     * @return: returns 0 if the user already exists
+     *                  1 if the user was inserted
+     * */
     addUser: function (user) {
         var collection = database.collection('users');
         //check to see if user already exists
-        return collection.updateOne(user, user, {upsert: true}).then(function (result) {
+        return collection.updateOne(
+            //check for matching username
+            {"username": user.username},
+            //only use this if it was created
+            {$setOnInsert: user},
+            //if not exist, create
+            {upsert: true}
+        ).then(function (result) {
             return result.upsertedCount;
         });
     },
 
     updateUser: function (oldUser, newUser) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get users collection
-        //         //todo: update oldUser with newUser
-        //
-        //         //todo: close db
-        //     }
-        // })
+
     },
 
-    submitNotebook: function (notebook, user) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err)
-        //     } else {
-        //         //todo: get notebooks collection
-        //         //todo: create notebook document
-        //         //todo: insert notebook document
-        //
-        //         //todo: get submissions collection
-        //         //todo: create submission document
-        //         //todo: insert submission document
-        //
-        //         //todo: close db
-        //     }
-        // });
+    /*
+     * @param submission: submission object according to specification above
+     * @param user: user object according to specification above
+     *
+     * Method adds a submission to the submissions collection then appends
+     * the title of the submission to the user's 'submissions' array.
+     *
+     * @return: if the submission already exists, then an object with an
+     * 'error' attribute will be returned.
+     * Else, true is returned
+     * */
+    addSubmission: function (submission, user) {
+        var submissionsCollection = database.collection("submissions");
+        //add submission. If submission already exists, don't add, return error
+        return submissionsCollection.updateOne({
+                "title": submission.title,
+                'author': user.username
+            },
+            submission, {upsert: true})
+
+            .then(function (result) {
+                if (!result.upsertedCount) {
+                    return {error: "submission already exists"};
+                }
+                var usersCollection = database.collection("users");
+                //Update user's 'submissions' array
+                return usersCollection.updateOne({"username": user.username}, {
+                    $addToSet: {
+                        "submissions": submission.title
+                    }
+                }).then(function () {
+                    return true;
+                });
+            });
     },
 
-    editNotebook: function (notebook, user) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get notebooks collection
-        //         //todo: find document
-        //         //todo: add notebook to list
-        //
-        //         //todo: close db
-        //     }
-        // });
+    editSubmission: function (submission, user) {
+
     },
 
     deleteSubmission: function (submission) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(Err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find submission
-        //         //todo: copy to deleted collection
-        //         //todo: remove from submissions collection
-        //
-        //         //todo: close db
-        //     }
-        // })
+
     },
 
     submitComment: function (submissionTitle, user, comment) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find submission document
-        //         //todo: create comment object
-        //         //todo: add 'comment' to list
-        //
-        //         //todo: close db
-        //     }
-        // })
+
     },
 
     submitReply: function (submissionTitle, user, comment, inReplyTo) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err)
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find inReplyTo comment
-        //         //todo: add comment to inReplyTo's comments
-        //
-        //         //todo: close db
-        //     }
-        // })
+
     },
 
     deleteComment: function (submissionTitle, user, comment) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: get submission document
-        //         //todo: find and remove comment
-        //
-        //         //todo: close db
-        //     }
-        // })
+
     },
 
     // Voting
-    upvote: function (notebook, user) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection,
-        //         //todo: find submission document
-        //         //todo: increment 'votes'
-        //         //todo: add 'user' to upvotedUsers
-        //         //todo: remove 'user' from downvotedUsers (if needed)
-        //
-        //         //todo: close db
-        //
-        //     }
-        // })
+    upvote: function (submission, user) {
+
     },
 
-    revokeUpvote: function (notebook, user) {
-        // MongoClient(url, function (err, db) {
-        //     if(err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find submission document
-        //         //todo: find 'user' in upvotedUsers
-        //         //todo: remove 'user' from upvotedUsers
-        //         //todo: decrement 'votes'
-        //
-        //         //todo: close db
-        //     }
-        // })
+    revokeUpvote: function (submission, user) {
+
     },
 
-    downvote: function (notebook, user) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find submission document
-        //         //todo: decrement 'votes'
-        //         //todo: add 'user' to downvotedUsers
-        //         //todo: remove 'user' from upvotedUsers (if needed)
-        //
-        //         //todo: close db
-        //
-        //     }
-        // })
+    downvote: function (submission, user) {
+
     },
 
-    revokeDownvote: function (notebook, user) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if(err){
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find 'user' in downvotedUsers
-        //         //todo: remove 'user'
-        //         //todo: increment 'votes'
-        //
-        //         //todo: close db
-        //     }
-        // })
+    revokeDownvote: function (submission, user) {
+
     },
 
-// GET methods
-    getNotebook: function (notebookTitle) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get notebooks collection
-        //         //todo: find notebook document
-        //
-        //         //todo: close db
-        //
-        //         //todo: return notebook
-        //     }
-        // });
-    },
+    // GET methods
 
     getSubmission: function (submissionTitle) {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: find submission document
-        //         //todo: create submission object
-        //
-        //         //todo: close db
-        //
-        //         //todo: return submission object
-        //     }
-        // })
+
     },
 
     getAllSubmissions: function () {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get submissions collection
-        //         //todo: iterate over all submission documents
-        //         //todo: create submission object and add to array
-        //
-        //         //todo: close db
-        //
-        //         //todo: return submissions array
-        //     }
-        // });
+
     },
 
     getUser: function (uname) {
@@ -315,18 +192,6 @@ module.exports = {
     },
 
     getAllUsers: function () {
-        // MongoClient.connect(url, function (err, db) {
-        //     if (err) {
-        //         return console.dir(err);
-        //     } else {
-        //         //todo: get users collection
-        //         //todo: iterate over all user documents
-        //         //todo: create user objects and add to array
-        //
-        //         //todo: close db
-        //
-        //         //todo: return array of user objects
-        //     }
-        // });
+
     }
 };
