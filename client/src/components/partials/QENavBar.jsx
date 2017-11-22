94d6c7e1c4dd928df31298ea2cac2572c20f8d5b
DrDrij <dr.drij@gmail.com>
2017-11-22 19:08:21 +0200

    Adjusted HTML and CSS for the new QE Menubar layout

--- client/src/components/partials/QENavBar.jsx
+++ client/src/components/partials/QENavBar.jsx
@@ -6,36 +6,23 @@ import logo from '../../assets/img/qe-logo-horizontal.png';
 class QENavBar extends Component {
     render() {
         return (
-            // <div className='row column'>
-                <div className="qe-nav">
-                    <p className="qe-logo">
-                        <a href="https://quantecon.org/" title="quantecon.org">
-                            <img src={logo} alt="QuantEcon logo"/>
-                            Quant<span>Econ</span>
-                        </a>
-                    </p>
-                    <ul className="qe-menu">
-                        <li>
-                            <a href="https://lectures.quantecon.org/">Lectures</a>
-                        </li>
-                        <li>
-                            <a href="https://quantecon.org/quantecon-py">QuantEcon.Py</a>
-                        </li>
-                        <li>
-                            <a href="https://quantecon.org/quantecon-jl">QuantEcon.jl</a>
-                        </li>
-                        <li>
-                            <a href="https://cheatsheets.quantecon.org/">Cheatsheets</a>
-                        </li>
-                        <li>
-                            <a href="http://discourse.quantecon.org/">Forum</a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/QuantEcon/">Repository</a>
-                        </li>
-                    </ul>
-                </div>
-            //</div>
+        <div class="qe-menubar">
+        
+            <p class="qe-menubar-logo"><a href="https://quantecon.org/" title="quantecon.org"><img src="{logo}" alt="QuantEcon logo"> Quant<span>Econ</span></a></p>
+        
+            <ul class="qe-menubar-nav">
+                <li><a href="https://lectures.quantecon.org/" title="Lectures">Lectures</a></li>
+                <li><a href="https://quantecon.org/python_index.html" title="QuantEcon.py">QuantEcon.py</a></li>
+                <li><a href="https://quantecon.org/julia_index.html" title="QuantEcon.jl">QuantEcon.jl</a></li>
+                <li><a href="http://notebooks.quantecon.org/" title="Notebooks">Notebooks</a></li>
+                <li><a href="http://cheatsheets.quantecon.org/" title="Cheatsheets">Cheatsheets</a></li>
+                <li><a href="https://quantecon.org/workshops" title="Workshops">Workshops</a></li>
+                <li><a href="http://discourse.quantecon.org/" title="Forum">Forum</a></li>
+                <li><a href="https://github.com/QuantEcon/" title="Repository"><span class="show-for-sr">Repository</span></a></li>
+                <li><a href="https://twitter.com/quantecon" title="Twitter"><span class="show-for-sr">Twitter</span></a></li>
+            </ul>
+
+        </div>
         )
     }
 }
