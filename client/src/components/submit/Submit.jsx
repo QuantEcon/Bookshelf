c95bbb7b9d2b8fb44a1281bfc8729a4aa2a77f04
Matt McKay <mamckay@gmail.com>
2018-04-11 10:36:04 +1000

    add deletion of account notification to submission acknowledgement

--- client/src/components/submit/Submit.jsx
+++ client/src/components/submit/Submit.jsx
@@ -423,6 +423,10 @@ class Submit extends Component {
                                     5. QuantEcon reserves the right the delete your notebook if found to violate
                                     these terms and conditions
                                 </li>
+                                <li>
+                                    6. If you choose to delete your account, your submissions and comments will 
+                                    remain listed on the forum and in any backups required to maintain the site.
+                                </li>
                             </ul>
 
                         </label>
