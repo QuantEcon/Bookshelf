440bc7b87165ab043641b6eae10a087bbd87e60e
Matt McKay <mamckay@gmail.com>
2018-05-24 10:26:46 +1000

    update title chars to 120

--- client/src/components/submit/Submit.jsx
+++ client/src/components/submit/Submit.jsx
@@ -479,7 +479,7 @@ class Submit extends Component {
                                         type="text"
                                         placeholder='Notebook Title'
                                         required='required'
-                                        maxLength="80"
+                                        maxLength="120"
                                         defaultValue={this.formData.title}
                                         onChange={this.titleChanged}/> {this.errors.title
                                         ? <p className="error-help-text">
