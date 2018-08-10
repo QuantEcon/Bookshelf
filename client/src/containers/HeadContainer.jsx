379eaf04f182423ea3ef05dcebceaf1285f81b1d
Lin  Peng <lin.peng@anu.edu.au>
2018-08-10 14:12:40 +1000

    notification for users after clicking invite button #174

--- client/src/containers/HeadContainer.jsx
+++ client/src/containers/HeadContainer.jsx
@@ -2,13 +2,24 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
 import Head from '../components/partials/Head';
-import {resetSearchParams} from '../actions/submissionList'
+import {resetSearchParams} from '../actions/submissionList';
+import CheckmarkIcon from 'react-icons/lib/fa/check-circle-o';
+
 class HeadContainer extends Component {
 
     constructor(props){
         super(props)
+        this.state = {
+          sentSuccess: null
+        };
+
+        this.handleSentSuccess = this.handleSentSuccess.bind(this)
         this.resetParams = this.resetParams.bind(this)
     }
+
+    handleSentSuccess = (truthValue) => {
+       this.setState({sentSuccess: truthValue});
+     }
 
     resetParams = () => {
         console.log("[HeadContainer] - reset search params")
@@ -19,11 +30,22 @@ class HeadContainer extends Component {
         return (
             <div>
                 <Head
+                    emailSuccess={this.handleSentSuccess}
                     isSignedIn={this.props.isSignedIn}
                     user={this.props.user}
                     history={this.props.history}
                     resetSearchParams={this.resetParams}
                     showAdmin={this.props.isAdmin}/>
+                    { this.state.sentSuccess && this.state.sentSuccess.sentValue ?
+                    <div className="success callout">
+                        <div className="container">
+                            <p className="callout-message">
+                                <CheckmarkIcon/>
+                              Email was successfully sent to <b>{this.state.sentSuccess.sentEmail}</b>
+                            </p>
+                        </div>
+                    </div>
+                : null }
             </div>
         )
     }
