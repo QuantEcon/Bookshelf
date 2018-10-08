16e6b6362b945b37040ba5c99da4bab530891508
Lin  Peng <lin.peng@anu.edu.au>
2018-10-08 15:30:27 +1100

    added tabs for admin announcment

--- client/src/components/partials/AnnouncementsBanner.jsx
+++ client/src/components/partials/AnnouncementsBanner.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Markdown from '@nteract/markdown'
 import Moment from 'react-moment'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 export default class AnnouncementBanner extends Component {
     constructor(props){
@@ -10,6 +11,7 @@ export default class AnnouncementBanner extends Component {
             showEdit: false,
             editText: ""
         }
+        this.editTextChanged = this.editTextChanged.bind(this);
     }
 
     toggleEdit = (e) => {
@@ -60,18 +62,32 @@ export default class AnnouncementBanner extends Component {
                             {this.props.announcement
                                 ? <Moment date={this.props.announcement.date} format="dddd, DD MMMM YYYY, H:mm A"/>
                                 : null}
-
                             {this.props.showAdmin
                             ? <div>
                                 {this.state.showEdit ?
                                     <div>
                                         {/* Below markdown source will render the editText on the page*/}
                                         {/*<Markdown source={this.state.editText} />*/}
-                                        <textarea name="editAnnouncement"
-                                        id="editAnnouncementTextArea"
-                                        placeholder="You can use markdown here..."
-                                        onChange={this.editTextChanged}/>
-                                        <Markdown source={this.state.editText} />
+                                        <Tabs>
+                                          <TabList>
+                                            <Tab>Write</Tab>
+                                            <Tab>Preview</Tab>
+                                          </TabList>
+
+                                          <TabPanel>
+                                            <textarea name="editAnnouncement"
+                                            id="editAnnouncementTextArea"
+                                            placeholder="You can use markdown here..."
+                                            defaultValue={this.state.editText}
+                                            onChange={this.editTextChanged}/>
+                                          </TabPanel>
+                                          <TabPanel>
+                                            <Markdown
+                                              source={this.state.editText
+                                              ? this.state.editText
+                                              : '*No description*'}/>
+                                          </TabPanel>
+                                        </Tabs>
                                         <button onClick={this.toggleEdit}>Cancel</button>
                                         <button onClick={this.submit}>Submit</button>
                                     </div>
