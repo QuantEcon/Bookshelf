0437fd45ff71a36cf8bd063e7d9304c036c1e4d3
Aakash <aakashgfude@gmail.com>
2018-12-27 16:21:49 +1100

    writing migration to make lastUpdated date same as published date

---
+++ server/js/db/migrations/1545887002911-setting-lastUpdated-to-published.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// modules ==================================
+var mongoose = require('mongoose');
+var config = require('./../_config');
+var Submission = require('./../models/Submission.js');
+
+// config ===================================
+const options = {
+  autoReconnect: true,
+  reconnectTries: 5,
+  reconnectInterval: 2000
+}
+
+async function update(data) {
+    await Promise.all(data.map(async(d) => {
+        await Submission.updateOne({ _id: d._id }, { $set: { lastUpdated: d.published }})
+    }))
+}
+
+module.exports.up = function (next) {
+    return mongoose.connect(config.url, options).then(db => {
+        return Submission.find({$where : "this.published > this.lastUpdated"});
+    })
+    .then((data) => {
+      update(data).then(() => {
+        mongoose.connection.close()
+        return next()
+      })
+    })
+    .catch(err => next(err))
+}
+
+module.exports.down = function (next) {
+    return mongoose.connect(config.url, options).then(db => {
+        // perform db operations here
+    })
+    .then(() => {
+      mongoose.connection.close()
+      return next()
+    })
+    .catch(err => next(err))
+}
