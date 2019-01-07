d0bb4524f0a21250a6cc377563f4a17e7477ecbe
AakashGC <aakashguptachoudhury@gmail.com>
2019-01-07 18:16:30 +1100

    formatting comment

--- server/app.js
+++ server/app.js
@@ -66,15 +66,20 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({extended: true, limit: '50mb', parameterLimit: 50000}));
 
 
-// Enable reverse proxy support in Express. This causes the
-// the "X-Forwarded-Proto" header field to be trusted so its
-// value can be used to determine the protocol. See 
-// http://expressjs.com/api#app-settings for more details.
+/**
+ * Enable reverse proxy support in Express. This causes the
+ * the "X-Forwarded-Proto" header field to be trusted so its
+ * value can be used to determine the protocol. See 
+ * http://expressjs.com/api#app-settings for more details.
+ * 
+ */ 
 app.enable('trust proxy');
 
-// Add a handler to inspect the req.secure flag (see 
-// http://expressjs.com/api#req.secure). This allows us 
-// to know whether the request was via http or https.
+/**
+ *  Add a handler to inspect the req.secure flag This allows us 
+ *  to know whether the request was via http or https.
+ */ 
+ 
 app.use (function (req, res, next) {
     console.log(process.env.NODE_ENV, " what is process env")
         if (req.secure) {
