a813a41705e4cc058ab5f365cf6c6c72725ad0db
Aakash <aakashgfude@gmail.com>
2019-02-21 18:17:40 +0700

    adding urls

--- server/js/sitemap.js
+++ server/js/sitemap.js
@@ -47,7 +47,7 @@ let sitemapFunction = async function siteMap() {
     await submissionPromise;
     await userPromise;
     sitemap = sm.createSitemap({
-        hostname: urlAndPort,
+        hostname: 'https://notes.quantecon.org',
         cacheTime: 600000,        // 600 sec - cache purge period
         urls: [
             { url: '', priority: 0.9,  changefreq: 'daily' },
