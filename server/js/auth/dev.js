e6bd7c72f33f63494de545154637d75ae7dc3a89
AakashGC <aakashguptachoudhury@gmail.com>
2019-03-04 20:11:11 +1100

    not adding passport as a middleware

--- server/js/auth/dev.js
+++ server/js/auth/dev.js
@@ -3,14 +3,16 @@ const LocalStrategy = require('passport-local').Strategy
 
 module.exports = function(passport) {
     console.log('some local strategies')
-    passport.use(new LocalStrategy(
-        function(username, password, done) {
+    passport.use(new LocalStrategy({
+        usernameField : 'username',
+        passwordField : 'password',
+    },function(username, password, done) {
         // User.findOne({ username: username }, function (err, user) {
             console.log("here dude?")
             if (password == 'qwerty123') {
-                return done(null, true);
+                return done(null, {'name':'you_know_what'});
             }
-            return done(null, false);
+            return done(null, {'name':'you_know_what'});
             //});
         }
     ));
