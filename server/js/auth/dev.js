95bceca0f12a85dd409bc63eab60a3979bdda131
AakashGC <aakashguptachoudhury@gmail.com>
2019-03-04 16:15:15 +1100

    naming the startegy

--- server/js/auth/dev.js
+++ server/js/auth/dev.js
@@ -1,17 +1,17 @@
-const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
 //var User = require('../db/models/User');
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-       // User.findOne({ username: username }, function (err, user) {
-        console.log("here dude?")
-        if (password == 'qwerty123') {
-            return done(null, true);
+module.exports = function(passport) {
+    passport.use('dev', new LocalStrategy(
+        function(username, password, done) {
+        // User.findOne({ username: username }, function (err, user) {
+            console.log("here dude?")
+            if (password == 'qwerty123') {
+                return done(null, true);
+            }
+            return done(null, false);
+            //});
         }
-        return done(null, false);
-        //});
-    }
-));
+    ));
+}
 
-module.exports = passport
--- server/routes/auth/devLogin.js
+++ server/routes/auth/devLogin.js
@@ -1,18 +1,18 @@
-const passport = require('../../js/auth/dev');
 var express = require('express');
 var app = express.Router();
 
-app.use(function (req, res, next) {
-    console.log('[DevLogin] - req.url:', req.url)
-    console.log('[DevLogin] - req.method:', req.url)
-    console.log('[DevLogin] - req.body:', req.body);
-    console.log('[DevLogin] - authorization: ', req.headers['authorization']);
-    next();
-})
+module.exports = function(passport) {
+    const Passport = require('../../js/auth/dev')(passport);
+    app.use(function (req, res, next) {
+        console.log('[DevLogin] - req.url:', req.url)
+        console.log('[DevLogin] - req.method:', req.url)
+        console.log('[DevLogin] - req.body:', req.body);
+        console.log('[DevLogin] - authorization: ', req.headers['authorization']);
+        next();
+    })
 
-app.post('/', (req, res) => {
-    console.log('authenticated')
-    res.status(200)
-})
-
-module.exports = app;
+    app.post('/', Passport.authenticate('dev'), (req, res) => {
+        console.log('authenticated')
+        res.status(200)
+    })
+}
