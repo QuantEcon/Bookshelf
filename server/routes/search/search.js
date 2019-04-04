f8509c2d7025f48c22e026d58496df1833c11bb9
Atlas <u6071322@anu.edu.au>
2019-04-04 18:51:25 +1100

    move sorting func to js folder

--- server/routes/search/search.js
+++ server/routes/search/search.js
@@ -10,45 +10,13 @@ var Comment = require('../../js/db/models/Comment');
 
 var isAuthenticated = require('../auth/isAuthenticated').isAuthenticated;
 var config = require('../../_config');
-// var changeOrderRandomly = require('../../lib/sorting').changeOrderRandomly;
+var changeOrderRandomly = require('../../js/sorting').changeOrderRandomly;
 
 let globallyStoredSearchParams = {}
 let globallyStoredCollections = {}
 
 async function getStoredData() {
     return await globallyStoredCollections;
-}
-
-/**
- *  Function to change the order of notebook randomly with a given probability
- * 
- * @param {probability with which you want to swap a notebook} prob 
- * @param {the present index to operate on} currentIndex
- * @param {total number of notebooks} totalNo 
- * @param {which indexes have been visited and operated upon} visitedArray 
- * @param {the notebook data} data 
- */
-function changeOrderRandomly(prob, currentIndex, totalNo, visitedArray, data) {
-  let randomNumber = Math.round(Math.random()*(totalNo - 1))
-  if (visitedArray.length < totalNo) {
-      while (visitedArray.includes(randomNumber)) {
-          randomNumber = Math.round(Math.random()*(totalNo - 1))
-      }
-  } else {
-      return;
-  }
-  let changeIndex = (Math.random() <= prob)
-  if (changeIndex) {
-      let temp = data[randomNumber]
-      data[randomNumber] = data[currentIndex]
-      data[currentIndex] = temp;
-      visitedArray.push(randomNumber);
-      visitedArray.push(currentIndex);
-  } else {
-      if (!visitedArray.includes(currentIndex)) {
-          visitedArray.push(currentIndex)
-      }
-  }
 }
 
 /**
