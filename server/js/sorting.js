20933955e55b88a5f6c23611789c83b33d8ad762
Atlas <u6071322@anu.edu.au>
2019-04-04 18:54:28 +1100

    made change to sorting func

--- server/js/sorting.js
+++ server/js/sorting.js
@@ -7,7 +7,7 @@
  * @param {which indexes have been visited and operated upon} visitedArray 
  * @param {the notebook data} data 
  */
-function changeOrderRandomly(prob, currentIndex, totalNo, visitedArray, data) {
+var changeOrderRandomly = function (prob, currentIndex, totalNo, visitedArray, data) {
     let randomNumber = Math.round(Math.random()*(totalNo - 1))
     if (visitedArray.length < totalNo) {
         while (visitedArray.includes(randomNumber)) {
